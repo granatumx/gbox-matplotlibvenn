a30b8194cfc74da32a9fecd1218280d48eeaaa90
Ubuntu <zjing@umail.ucsb.edu>
2021-01-31 19:26:32 +0000

    Updated...

--- matplotlibvenn.py
+++ matplotlibvenn.py
@@ -41,7 +41,7 @@ def main():
     else:
         venn3(packedsets, set_labels=(labelSet1, labelSet2, labelSet3))
 
-    gn.add_current_figure_to_results(caption, zoom=2, dpi=50)
+    gn.add_current_figure_to_results(caption, dpi=300)
 
     gn.commit()
 
