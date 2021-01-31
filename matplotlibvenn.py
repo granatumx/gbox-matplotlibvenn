953b81273c4a11b78c2c4b8ea9ec61a63126e05f
Ubuntu <zjing@umail.ucsb.edu>
2021-01-31 22:52:40 +0000

    Updated...

--- matplotlibvenn.py
+++ matplotlibvenn.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from granatum_sdk import Granatum
 from matplotlib_venn_wordcloud import venn3_wordcloud
-from matplotlib_venn import venn3
+from matplotlib_venn import venn3, venn3_circles
 import matplotlib.patheffects as path_effects
 
 
